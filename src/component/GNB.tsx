891ea24140f6fc5b142734c9ca092db9301a5c0b
hin6150 <hin6150@gmail.com>
2024-01-31 20:31:38 +0900

    :tada: 프로젝트 시작

---
+++ src/component/GNB.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logoImage from '../assets/images/logo.png';
+
+const GNB = () => {
+  return (
+    <div>
+      <img src={logoImage} width={120} style={{ padding: 24 }} />
+    </div>
+  );
+};
+
+export default GNB;
